386d02601ea4fe26d2601a7b3ca022b13e0cc194
Suphan Khaomoon <jack231029@gmail.com>
2021-03-17 20:12:23 +0700

    1st branch commit

--- Employee.java
+++ Employee.java
@@ -5,13 +5,6 @@ public class Employee
 	public String firstname;
 	public String lastname;
 	private int salary;
-// 13.2 	
-//		1. เพิ่มตัวแปร
-	public String position;
-//		2. เขียน method เพิ่ม
-	public void checkPosition () {
-		System.out.println("My Position is " + position);
-	}
 
 	public static void main (String[] args) {
 // 10. 3 - Lab 3: New Class
@@ -30,23 +23,13 @@ public class Employee
 		System.out.println(employees[0].salary);
 		System.out.println(employees[1].salary);
 		System.out.println(employees[2].salary);
-		
-		System.out.println();
-		
-//		3. ทดสอบว่า method checkPosition ทำงานถูกป่าว
-//		3.1 สร้าง method main ขึ้นมา
-//		3.2 สร้าง instance ของ Employee ขึ้นมาซักอัน
-		Employee em = new Employee("Suphan", "Khaomoon", 15000, "Programmer");
-		
-//		3.3 เรียก method checkPosition ของ Employee ที่สร้างขึ้น
-		em.checkPosition();
+
 	}
 	
-//	ทดลอง Constructor Overloading
 	public Employee () {
 		this.firstname = "Anonymous";
 		this.lastname = "Anonymous";
-		this.position = "Anonymous";
+//		this.position = "Anonymous";
 		this.salary = 9000;
 	}
 	
@@ -62,12 +45,12 @@ public class Employee
 		this.salary = salaryInput;
 	}
 	
-	public Employee (String firstnameInput, String lastnameInput, int salaryInput, String positionInput) {
-		this.firstname = firstnameInput;
-		this.lastname = lastnameInput;
-		this.salary = salaryInput;
-		this.position = positionInput;
-	}
+//	public Employee (String firstnameInput, String lastnameInput, int salaryInput, String positionInput) {
+//		this.firstname = firstnameInput;
+//		this.lastname = lastnameInput;
+//		this.salary = salaryInput;
+//		this.position = positionInput;
+//	}
 	
 	public void hello () {
 		System.out.println("Hello " + firstname);
