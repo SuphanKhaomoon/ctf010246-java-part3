ba07a44767625d04a0b8aca4083cdff7335c646a
Suphan Khaomoon <jack231029@gmail.com>
2021-03-17 20:04:12 +0700

    1st commit

---
+++ Employee.java
@@ -0,0 +1,80 @@
+package CTF01.Java.part3;
+
+public class Employee
+{
+	public String firstname;
+	public String lastname;
+	private int salary;
+// 13.2 	
+//		1. เพิ่มตัวแปร
+	public String position;
+//		2. เขียน method เพิ่ม
+	public void checkPosition () {
+		System.out.println("My Position is " + position);
+	}
+
+	public static void main (String[] args) {
+// 10. 3 - Lab 3: New Class
+//	- ทดลองสร้าง Array จาก Class Employee
+		Employee[] employees = new Employee[3];
+		for (int i = 0; i < employees.length; i++)
+		{
+			employees[i] = new Employee("Firstname " + i, "Lastname " + i, 10000 * (i + 1));
+		}
+		System.out.println(employees[0].firstname);
+		System.out.println(employees[1].firstname);
+		System.out.println(employees[2].firstname);
+		System.out.println(employees[0].lastname);
+		System.out.println(employees[1].lastname);
+		System.out.println(employees[2].lastname);
+		System.out.println(employees[0].salary);
+		System.out.println(employees[1].salary);
+		System.out.println(employees[2].salary);
+		
+		System.out.println();
+		
+//		3. ทดสอบว่า method checkPosition ทำงานถูกป่าว
+//		3.1 สร้าง method main ขึ้นมา
+//		3.2 สร้าง instance ของ Employee ขึ้นมาซักอัน
+		Employee em = new Employee("Suphan", "Khaomoon", 15000, "Programmer");
+		
+//		3.3 เรียก method checkPosition ของ Employee ที่สร้างขึ้น
+		em.checkPosition();
+	}
+	
+//	ทดลอง Constructor Overloading
+	public Employee () {
+		this.firstname = "Anonymous";
+		this.lastname = "Anonymous";
+		this.position = "Anonymous";
+		this.salary = 9000;
+	}
+	
+	public Employee (String firstnameInput, String lastnameInput) {
+		this();
+		this.firstname = firstnameInput;
+		this.lastname = lastnameInput;
+	}
+	
+	public Employee (String firstnameInput, String lastnameInput, int salaryInput) {
+		this.firstname = firstnameInput;
+		this.lastname = lastnameInput;
+		this.salary = salaryInput;
+	}
+	
+	public Employee (String firstnameInput, String lastnameInput, int salaryInput, String positionInput) {
+		this.firstname = firstnameInput;
+		this.lastname = lastnameInput;
+		this.salary = salaryInput;
+		this.position = positionInput;
+	}
+	
+	public void hello () {
+		System.out.println("Hello " + firstname);
+	}
+	
+	public int getSalary () {
+		return salary;
+	}
+	
+}
